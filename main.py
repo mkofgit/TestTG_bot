0869681b0bae7d97d91f627bc866fe4b0a943e43
Mikhail <115636158+mkofgit@users.noreply.github.com>
2025-03-14 00:44:48 +0500

    26

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 
 # 🔹 ДАННЫЕ ДЛЯ ТЕЛЕГРАМА
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")  # Хранить в переменных окружения!
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")  # Чат для ошибок
 
 # 🔹 ДАННЫЕ ДЛЯ EMAIL (Хранить в Railway Variables)
 SMTP_SERVER = "smtp.mail.ru"  # Используем Mail.ru
@@ -16,6 +15,10 @@ SMTP_PORT = 587
 SMTP_LOGIN = "unityspace2024@mail.ru"  # Твоя почта
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")  # Пароль приложения
 JIVO_EMAIL = "idmurgpsfrtnjivosite@jivo-mail.com"  # Email-канал Jivo
+
+# Вывод для проверки
+print(f"Telegram Token: {TELEGRAM_TOKEN}")
+print(f"SMTP Password: {SMTP_PASSWORD}")
 
 @app.route('/telegram_webhook', methods=['POST'])
 def telegram_webhook():
@@ -27,10 +30,7 @@ def telegram_webhook():
         user_text = data["message"].get("text", "")
 
         # 🔹 Отправляем в Jivo
-        success = send_to_jivo_email(user_text)
-
-        if not success:
-            send_telegram_log(f"🚨 Ошибка отправки Email в Jivo:\n\n{user_text}")
+        send_to_jivo_email(user_text)
 
     return jsonify({"status": "ok"})
 
@@ -47,19 +47,8 @@ def send_to_jivo_email(user_message):
             server.login(SMTP_LOGIN, SMTP_PASSWORD)
             server.sendmail(SMTP_LOGIN, JIVO_EMAIL, msg.as_string())
         print("✅ Email отправлен в Jivo")
-        return True
     except Exception as e:
         print(f"🚨 Ошибка отправки Email: {str(e)}")
-        return False
-
-def send_telegram_log(text):
-    """Отправка ошибки в Telegram"""
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    payload = {"chat_id": TELEGRAM_CHAT_ID, "text": text, "parse_mode": "Markdown"}
-    try:
-        requests.post(url, json=payload)
-    except Exception as e:
-        print(f"🚨 Ошибка отправки лога в Telegram: {str(e)}")
 
 if __name__ == '__main__':
     from waitress import serve
